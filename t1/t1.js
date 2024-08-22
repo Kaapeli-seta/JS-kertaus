bbe26c21214b3b9a2b7ad2eab92e05607928b32a
Kaapo <142903354+Kaapeli-seta@users.noreply.github.com>
2024-08-22 09:40:31 +0300

    update

--- t1/t1.js
+++ t1/t1.js
@@ -1,5 +1,13 @@
-function greeting()
+function cel()
 {
   console.log("Hello world")
+
+  const C = parseFloat(prompt("Temperature in celsius>"))
+  console.log(C)
+
+  document.getElementById("demo").innerHTML = C + 273.15 + " K in Kelvin"
+  document.getElementById("demo2").innerHTML = (C * 9/5) + 32 + " F in Fahrenheit"
+  return
 }
-greeting();
+
+cel();
